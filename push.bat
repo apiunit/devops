7330d52ab130727248ad9af9cd831cef5ff390fe
softreck <info@softreck.com>
2020-09-15 14:10:16 +0200

    creating devops part of organization repositories

--- push.bat
+++ push.bat
@@ -1,43 +1,37 @@
-:: update data on git repo
-@ECHO OFF
-SetLocal EnableDelayedExpansion
-for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
-for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a
-@ECHO ON
-
 echo 'I will push all projects :'
 cd ..
 dir
 
 cd docs
-git add . && git commit -m "%message%" && git push origin master
+git push
 cd ..
 
 cd definition
-git add . && git commit -m "%message%" && git push origin master
+git push
 
 cd ..
 
 cd www
-git add . && git commit -m "%message%" && git push origin master
+git push
 
 cd ..
 
 cd logo
-git add . && git commit -m "%message%" && git push origin master
+git push
 
 cd ..
 
 cd schema
-git add . && git commit -m "%message%" && git push origin master
+git push
 
 cd ..
 
 cd examples
-git add . && git commit -m "%message%" && git push origin master
+git push
 
 cd ..
 
 echo 'I pushed all projects'
 dir
 cd devops
+git push
